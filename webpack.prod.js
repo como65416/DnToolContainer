cea2a1b0f2abb266e72c6a4742b5f94877b274e7
comoco <como65416@gmail.com>
2018-12-27 01:10:28 +0800

    webpack.prod.js add UglifyJsPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -43,6 +43,20 @@ module.exports = {
   },
   optimization: {
     minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          warnings: false,
+          parse: {},
+          compress: {},
+          mangle: true,
+          output: null,
+          toplevel: false,
+          nameCache: null,
+          ie8: false,
+          keep_fnames: false,
+          sourceMap: true
+        }
+      }),
       new OptimizeCSSAssetsPlugin
     ]
   },
