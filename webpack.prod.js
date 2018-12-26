888e44883a1856672fdb684910fa703f888d05be
comoco <como65416@gmail.com>
2018-12-26 23:35:22 +0800

    add webpack.prod.js

---
+++ webpack.prod.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { VueLoaderPlugin } = require('vue-loader');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  mode: 'production',
+  target: 'electron-renderer',
+  entry: [
+    './webpack-main.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, './public/dist'),
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+      },
+      {
+        test: /\.vue$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['vue-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=50000'
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `bundle.css`,
+    }),
+    new VueLoaderPlugin()
+  ]
+};
