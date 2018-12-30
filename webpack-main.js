5bccc33599d98792ebbd333133eb3a32bdeb1664
comoco <como65416@gmail.com>
2018-12-30 15:59:01 +0800

    add Vuex store

--- webpack-main.js
+++ webpack-main.js
@@ -2,10 +2,24 @@ import Vue from 'vue'
 import App from './src/App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import Vuex from 'vuex'
 
 Vue.use(ElementUI);
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    isDevToolsEnabled: false
+  },
+  mutations: {
+    changeDevToolsEnableStatus (state, value) {
+      state.isDevToolsEnabled = value;
+    }
+  }
+});
 
 new Vue({
   el: '#app',
+  store,
   render: h => h(App)
 });
